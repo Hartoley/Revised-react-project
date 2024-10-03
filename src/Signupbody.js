088e9393f988352200e4135b886427da3dd4a20c
Hartoley <tolanijimoh1@gmail.com>
2024-10-03 10:32:01 +0100

    View toggle

--- src/Signupbody.js
+++ src/Signupbody.js
@@ -1,116 +1,172 @@
-import React, { useEffect, useState } from 'react'
-import { useFormik } from 'formik'
-import * as yup from 'yup'
-import axios from 'axios'
-import { ToastContainer, toast } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css';
-import { useNavigate } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import Studentlogin, {saving, successful, failed} from './Redux/Studentsredux' 
-import { useSelector, useDispatch } from 'react-redux'
-import './signupbody.css'
+import React, { useEffect, useState } from "react";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
+import "./signupbody.css";
 
 const Signupbody = () => {
-    const disptach = useDispatch() 
-    const endpoint = "https://react-node-project-3.onrender.com"
-    const navigate = useNavigate()
-    const [students, setstudents] = useState([])
+  const dispatch = useDispatch();
+  const endpoint = "https://react-node-project-3.onrender.com";
+  const navigate = useNavigate();
+  const [students, setStudents] = useState([]);
+  const [showPassword, setShowPassword] = useState(false);
 
+  useEffect(() => {
+    axios
+      .get(`${endpoint}/udemy/student/getdata`)
+      .then((res) => {
+        console.log("students data from API:", res.data);
+        setStudents(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-    useEffect(() => {
-      axios.get(`https://react-node-project-1.onrender.com/udemy/student/getdata`)
-        .then((res) => {
-          console.log("students data from API:", res.data);
-          setstudents(res.data); 
-          // console.log(res.data[0].username);
-          disptach(successful(res.data))
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }, []);
-    
-  
-  
-    const formik = useFormik({
-      initialValues:{
-        username:"",
-        email:"",
-        password:"",
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      email: "",
+      password: "",
     },
-      validationSchema: yup.object({
-        username: yup.string().min(4, 'username is too short').required('username is required'),
-        email: yup.string().email('must be a valid email').required('email is required'),
-        password: yup.string().min(5, 'password is too short').matches(`^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-])` , "password must have at least one capital letter, an integer and a special character").required('password is required')
-      }),
-  
-      onSubmit:(value)=>{
-        console.log(value);
-        
-        const existingstudents = students.find(exist=> exist.email == value.email || exist.password == value.password || exist.email == value.email)
-       
-        if (existingstudents) {
-          toast.error("user already exist")
-        }else{
-          axios.post(`https://react-node-project-1.onrender.com/udemy/student/register`, value)
-          .then((res)=>{
-              console.log("students signed up successful");
-              toast.success("students signed up successful")
-              navigate("/students/login")
-          }).catch((error)=>{
-            console.log(error);
-            toast.error("Signup failed")
+    validationSchema: yup.object({
+      username: yup
+        .string()
+        .min(4, "Username is too short")
+        .required("Username is required"),
+      email: yup
+        .string()
+        .email("Must be a valid email")
+        .required("Email is required"),
+      password: yup
+        .string()
+        .min(5, "Password is too short")
+        .matches(
+          `^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-])`,
+          "Password must have at least one capital letter, an integer, and a special character"
+        )
+        .required("Password is required"),
+    }),
+    onSubmit: (value) => {
+      console.log(value);
+      const existingStudent = students.find(
+        (exist) =>
+          exist.email === value.email || exist.password === value.password
+      );
+      if (existingStudent) {
+        toast.error("User already exists");
+      } else {
+        axios
+          .post(`${endpoint}/udemy/student/register`, value)
+          .then((res) => {
+            console.log("Students signed up successfully");
+            toast.success("Students signed up successfully");
+            navigate("/students/login");
           })
-        }
-  
+          .catch((error) => {
+            console.log(error);
+            toast.error("Signup failed");
+          });
       }
-    })
+    },
+  });
 
-
+  // Function to toggle password visibility
+  const togglePasswordVisibility = () => {
+    setShowPassword((prev) => !prev);
+  };
 
   return (
     <>
-        <form className="body" action="" id='body' onSubmit={formik.handleSubmit} >
-       
-            <div className="inputBox" id='bodydiv'>
-                <p>Sign up and start learning</p>
-                <div id="inputMainBox">
-                    <input onBlur={formik.handleBlur} onChange={formik.handleChange} name="username" type="text" placeholder='Full name'/>
-                    <p>{formik.touched.username && formik.errors.username? formik.errors.username:''}</p>
-                    <input onBlur={formik.handleBlur} onChange={formik.handleChange} name='email' type="text" placeholder='Email'/>
-                    <p>{formik.touched.email && formik.errors.email? formik.errors.email:''}</p>
-                    <input onBlur={formik.handleBlur} onChange={formik.handleChange} name='password' type="text" placeholder='Password'/>
-                    <p>{formik.touched.password && formik.errors.password? formik.errors.password:''}</p>
-                </div>
-                <div className="lineBox">
-                    <div className="lines"></div>
-                    <div className="lines"></div>
-                    <div className="lines"></div>
-                    <div className="lines"></div>
-                </div>
-
-                <button type='submit' className='buttonSignin'>Sign up</button>
-                <ToastContainer/>
-                <div className="termsBox" id='termsBox'>
-                    <p>By signing up, you agree to our <span>Terms of Use</span> and <span>Privacy Policy.</span></p>
-                </div>
-
-                <div id='loginBox' className="loginBox">
-                    <p>
-                        Already have an account? <span>
-                        <a href="/students/login">
-                                Log in
-                            </a>
-                        </span>
-                    </p>
-                </div>
+      <form className="body" action="" id="body" onSubmit={formik.handleSubmit}>
+        <div className="inputBox" id="bodydiv">
+          <p>Sign up and start learning</p>
+          <div id="inputMainBox">
+            <input
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              name="username"
+              type="text"
+              placeholder="Full name"
+            />
+            <p>
+              {formik.touched.username && formik.errors.username
+                ? formik.errors.username
+                : ""}
+            </p>
+            <input
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              name="email"
+              type="text"
+              placeholder="Email"
+            />
+            <p>
+              {formik.touched.email && formik.errors.email
+                ? formik.errors.email
+                : ""}
+            </p>
+            <div className="password-container">
+              <input
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+                name="password"
+                type={showPassword ? "text" : "password"}
+                placeholder="Password"
+              />
+              <span
+                onClick={togglePasswordVisibility}
+                className="password-toggle"
+                style={{ cursor: "pointer" }}
+              >
+                {showPassword ? (
+                  <AiFillEyeInvisible size={20} />
+                ) : (
+                  <AiFillEye size={20} />
+                )}
+              </span>
             </div>
-            
-            
-           
-            </form>
-    </>
-  )
-}
+            <p>
+              {formik.touched.password && formik.errors.password
+                ? formik.errors.password
+                : ""}
+            </p>
+          </div>
+          <div className="lineBox">
+            <div className="lines"></div>
+            <div className="lines"></div>
+            <div className="lines"></div>
+            <div className="lines"></div>
+          </div>
 
-export default Signupbody
+          <button type="submit" className="buttonSignin">
+            Sign up
+          </button>
+          <ToastContainer />
+          <div className="termsBox" id="termsBox">
+            <p>
+              By signing up, you agree to our <span>Terms of Use</span> and{" "}
+              <span>Privacy Policy.</span>
+            </p>
+          </div>
+
+          <div id="loginBox" className="loginBox">
+            <p>
+              Already have an account?{" "}
+              <span>
+                <a href="/students/login">Log in</a>
+              </span>
+            </p>
+          </div>
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default Signupbody;
