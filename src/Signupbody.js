7f9a563d62a78897ec4345c15bfd2eed127fe2bd
Hartoley <tolanijimoh1@gmail.com>
2024-10-09 12:25:29 +0100

    message

--- src/Signupbody.js
+++ src/Signupbody.js
@@ -87,7 +87,7 @@ const Signupbody = () => {
             console.log(error);
             setLoading(false);
             toast.dismiss();
-            toast.error("Signup failed");
+            toast.error(error?.response?.data.message);
           });
       }
     },
