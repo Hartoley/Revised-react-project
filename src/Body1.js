354baa27a7f58cc2f76524d1d8c6e12c37a049b4
Hartoley <tolanijimoh1@gmail.com>
2025-02-24 10:42:00 +0000

    Changed Background Image

--- src/Body1.js
+++ src/Body1.js
@@ -1,34 +1,36 @@
-import React from 'react'
-import './body1.css'
-
+import React from "react";
+import "./body1.css";
 
 const Body1 = () => {
   return (
     <>
-        <div className="body1">
-            <img className='img' src="https://img-b.udemycdn.com/notices/web_carousel_slide/image/db24b94e-d190-4d5a-b1dd-958f702cc8f5.jpg" alt="" />
-            <div className="whiteBox">
-                <p className='you'>Learning that gets you</p>
-                <p className='skills'>Skills for your present (and your future). Get started with us.</p>
-            </div>
-           
+      <div className="body1">
+        <img
+          className="img"
+          src="https://img-c.udemycdn.com/notices/web_carousel_slide/image/f013d2b2-58ce-4bad-be62-336b43b963f9.png"
+          alt=""
+        />
+        <div className="whiteBox">
+          <p className="you">Learning that gets you</p>
+          <p className="skills">
+            Skills for your present (and your future). Get started with us.
+          </p>
         </div>
-        <div className="dismis">
-            <div className="training">
-              <p>Training 5 or more people?Get your team access to Udemy's top 25,000+ courses</p>
-            </div>
-            <div className="buttonBox">
-              <button className="button1">Get Udemy Business</button>
-              <button className="button2">Dismiss</button>
-            </div>
-
-           
-            
+      </div>
+      <div className="dismis">
+        <div className="training">
+          <p>
+            Training 5 or more people?Get your team access to Udemy's top
+            25,000+ courses
+          </p>
         </div>
-        
-       
+        <div className="buttonBox">
+          <button className="button1">Get Udemy Business</button>
+          <button className="button2">Dismiss</button>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Body1
+export default Body1;
