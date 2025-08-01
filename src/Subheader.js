d3aae142abdd6efa522c88b944194bf1dcaa433f
Hartoley <tolanijimoh1@gmail.com>
2025-08-02 00:46:12 +0100

    `Refactored Subheader component and body1.css styles, updated subheader.css`

--- src/Subheader.js
+++ src/Subheader.js
@@ -1,27 +1,23 @@
-import React from 'react'
-import './subheader.css'
+import React from "react";
+import "./subheader.css";
 
 const Subheader = () => {
   return (
-    <>
-      <div className="subheader">
-        
-        <div className="dropDown">
-            <p>Development</p>
-            <p>Business</p>
-            <p>Finance & Accounting</p>
-            <p>IT & Software</p>
-            <p>Office Productivity</p>
-            <p>Personal Development</p>
-            <p>Design</p>
-            <p>Marketing</p>
-            <p>Health & Fitness</p>
-            <p>Music</p>
-        </div>
-        
+    <div className="subheader">
+      <div className="dropDown">
+        <p>Development</p>
+        <p>Business</p>
+        <p>Finance & Accounting</p>
+        <p>IT & Software</p>
+        <p>Office Productivity</p>
+        <p>Personal Development</p>
+        <p>Design</p>
+        <p>Marketing</p>
+        <p>Health & Fitness</p>
+        <p>Music</p>
       </div>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default Subheader
+export default Subheader;
